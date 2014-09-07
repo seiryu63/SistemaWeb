3d9a35eb0cba51f7c600f00784d2dce8cf80460c
seiryu63 <veta6363@gmail.com>
2014-09-07 15:51:00 -0600

    Munu funcionando con estilos y plantilla definida.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new casamunoz\empresaBundle\casamunozempresaBundle(),
 
             new jQuery\jQueryBundle\jQueryjQueryBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
